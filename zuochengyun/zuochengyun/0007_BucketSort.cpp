681d419b09698e5d8f1a613d7b61643fe7ca0c11
caoduanxin <13622150883@163.com>
2020-08-11 16:21:03 +0800

    桶排序及其应用

---
+++ zuochengyun/zuochengyun/0007_BucketSort.cpp
@@ -0,0 +1,36 @@
+////桶排序：（计数排序实现）
+////	申请一个能够包含最大数的数组，依次统计原数组中每个数出现的个数，最后遍历申请的
+////	数组，将统计的数字保存下来
+////	时间复杂度（O(n)）
+////	空间复杂度（O（n））
+////	稳定性排序
+////数据特性：非负整数
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//vector<int> bucketSort(vector<int> num) {
+//	if (num.size() < 2)
+//		return num;
+//	int a = -1;
+//	for (auto i: num) {
+//		a = i > a ? i : a;
+//	}
+//	vector<int> tmp(a + 1, 0);
+//	for (auto i : num) {
+//		tmp[i]++;
+//	}
+//	int j = 0;
+//	for (int i = 0; i < tmp.size(); i++) {
+//		while (tmp[i]-- > 0)
+//			num[j++] = i;
+//	}
+//	return num;
+//}
+//int main() {
+//	vector<int> num = { 8,8,7,7,6,6,5,5,4,3,2,1 };
+//	vector<int> res = bucketSort(num);
+//	for (auto i : res) {
+//		cout << i;
+//	}
+//	return 0;
+//}
